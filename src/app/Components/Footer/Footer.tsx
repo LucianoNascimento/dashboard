ece90c41b74299325facfc8a4303763373f90b32
Luciano Nascimento de França <lucianosantosti@hotmail.com>
2025-03-07 20:59:50 -0300

    inclusao do tailwind no backend (#3)

---
+++ src/app/Components/Footer/Footer.tsx
@@ -0,0 +1,31 @@
+import './Footer.css';
+import React from "react";
+
+const FooterComponent: React.FC = () => {
+  return (
+    <footer className='container-footer'>
+      <div className="contact">
+        <div className="contact-info">
+          <h2>Contatos</h2>
+          <p>Email: contato@exemplo.com</p>
+          <p>Telefone: (11) 12345-6789</p>
+          <p>Endereço: Rua Exemplo, 123, Cidade, Estado</p>
+        </div>
+        <div className="social-media">
+          <h2>Redes Sociais</h2>
+          <ul>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Instagram</a></li>
+          </ul>
+        </div>
+        <div className="additional-info">
+          <h2>Informações</h2>
+          <p>© 2024 Meu Site. Todos os direitos reservados.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default FooterComponent;
