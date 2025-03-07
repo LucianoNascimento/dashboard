61720faabef44d5ddf28aaa4b86129ca23dab3ba
LucianoNascimento <lucianosantosti@hotmail.com>
2025-03-07 01:22:20 -0300

    inclusao do tailwind no backend

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import "./globals.css";
+import React from "react";
+import HeaderComponent from "./Components/Header/Header";
+import FooterComponent from "./Components/Footer/Footer";
+import 'leaflet/dist/leaflet.css';
+import SectionComponent from "./Components/Section/Section";
+
+
+export default function Home() {
+  return (
+    <div>
+        <HeaderComponent/>
+            <link href="/inicio"/>
+            <link href="/dashboard"/>
+            <link href="/relatorios"/>
+            <link href="/gestaoUsuarios" />
+            <link href="/analiseDesempenho"/>
+            <link href="/configuracoes"/>
+            <link href="/feedbackClientes"/>
+        <SectionComponent/>
+        <FooterComponent/>
+    </div>
+  );
+}
